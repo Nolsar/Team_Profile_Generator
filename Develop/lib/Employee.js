7b7c89b85f5ae79194fcfac03a714a708bc5396e
Sara Nolan <saraa.ann28@gmail.com>
2021-02-27 17:08:22 -0500

    added employee class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -2,11 +2,28 @@
 
 // The first class is an `Employee` parent class with the following properties and
 // methods:
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
 
-//   * name
-//   * id
-//   * email
-//   * getName()
-//   * getId()
-//   * getEmail()
-//   * getRole() // Returns 'Employee'
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getemail() {
+        return this.email;
+    }
+
+    getRole() {
+        return 'Employee';
+    }
+}
+
+module.exports = Employee;
