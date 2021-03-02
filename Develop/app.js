789da729532a8c4acf1479615f1f4eada6dd4ddc
Sara Nolan <saraa.ann28@gmail.com>
2021-03-02 16:22:33 -0500

    added prompts for employee questions and code to build employees and render HTML

--- Develop/app.js
+++ Develop/app.js
@@ -1,82 +1,267 @@
-const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+var uniqueId = 0;
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const teamMembers = [];
 
-// The project must generate a `team.html` page in the `output` directory, that displays a nicely formatted team roster. Each team member should display the following in no particular order:
-
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-
-
-// after the questions of the current object are done, you call the initial question function
 
 //function to ask the initial question of who you are creating
-const employeePrompt = () =>
-  inquirer.prompt([
+const questions = [
+  //Role
+  {
+    type: 'list',
+    name: 'role',
+    message: "Select team member's role",
+    choices: [
+      'Engineer',
+      'Intern',
+      'Manager',
+    ]
+  },
+  //Name
+  {
+    type: 'input',
+    name: 'name',
+    message: "Enter team member's name",
+  },
+  //id
+  {
+    type: 'input',
+    name: 'id',
+    message: 'What is your id number?',
+  },
+  //Email
+  {
+    type: 'input',
+    name: 'email',
+    message: "Enter team member's email address",
+  },
+];
 
-    //Name
-    {
-      type: 'input',
-      name: 'name',
-      message: "Enter team member's name",
-    },
-    //Role
-    {
-      type: 'list',
-      name: 'role',
-      message: "Select team member's role",
-      choices: [
-        'Engineer',
-        'Intern',
-        'Manager',
-      ]
-    },
-    //ID
-    {
-      type: 'input',
-      name: 'id',
-      message: "Enter team member's ID",
-    },
-    //Email
-    {
-      type: 'input',
-      name: 'email',
-      message: "Enter team member's email address",
-    },
-  ])
-  //function for each of the object types
-    .then((response) => {
-      // console.log(response);
-      let roleInfo = response.role;
-      if (response.role === 'Engineer') {
-        roleInfo = 'Github username';
-      } else if (response.role === 'Intern') {
-        roleInfo = 'school'
-      } else {
-        roleInfo = 'office phone number'
-      }
-      inquirer.prompt([{
-        message: `Enter team member's ${roleInfo}`,
-        name: "roleInfo",
-      }])
-    });
+const managerQuestions = [{
+  type: 'input',
+  name: 'office',
+  message: 'What is your office number?'
+}];
 
-employeePrompt();
-  
-  //  
+const internQuestions = [{
+  type: 'input',
+  name: 'school',
+  message: 'What school did you go to?',
+}];
+
+const engineerQuestions = [{
+  type: 'input',
+  name: 'github',
+  message: 'What is your Github username',
+}];
+
+//add employees
+const moreEmployee = [{
+  name: 'confirm',
+  type: 'confirm',
+  message: 'Would you like to add more team members?',
+  default: false
+}];
+
+async function Question() {
+  try {
+    //first round of team member questions
+    let mainAnswers = await inquirer.prompt(questions);
+
+    // determines next set of question
+    let role = await sendToNextPrompt(mainAnswers);
+
+    // get the role specific questions
+    let roleAnswers = await inquirer.prompt(role);
+
+    // employee data compiled in an object 
+    let employeeData = await { ...mainAnswers, ...roleAnswers };
+
+    // takes the employee and builds with the appropriate constructor 
+    let employee = await buildEmployee(employeeData);
+
+    // push the created employee to the teamMembers array 
+    teamMembers.push(employee);
+
+    // ask if they would like to add more team members
+    let employeeAdd = await inquirer.prompt(moreEmployee);
+
+    // validate response for the next action
+    addMoreOrRender(employeeAdd.confirm);
+  }
+  catch (err) {
+    console.log(`There was an error somewhere in the async ${err}.`);
+  }
+};
+
+//function to return the specific role questions needed for the next prompt 
+function sendToNextPrompt(employee) {
+  employee.role = employee.role;
+
+  // console.log(employee); 
+
+  switch (employee.role) {
+    case 'Manager': return managerQuestions;
+    case 'Intern': return internQuestions;
+    case 'Engineer': return engineerQuestions;
+    default: return `Something went wrong! did you pick a role?`;
+  }
+};
+
+// builds a new Employee with the specific constructor
+function buildEmployee(employee) {
+  let name = employee.name;
+  let id = employee.id;
+  let email = employee.email;
+  let role = employee.role;
+
+  // console.log('build employee function', employee);
+
+  switch (role) {
+    case 'Manager': return new Manager(name, id, email, employee.officeNumber);
+    case 'Intern': return new Intern(name, id, email, employee.school);
+    case 'Engineer': return new Engineer(name, id, email, employee.gitHub);
+    default: return 'Something went wrong in building an employee';
+  }
+}
+
+function addMoreOrRender(confirm) {
+  if (confirm) {
+    // return back to the top of the questions to add in another employee 
+    return Question();
+  }
+  // checking to see if this directory exist 
+  fs.access(OUTPUT_DIR, (err) => {
+
+    if (err) {
+      // else we make the directory AND create the file 
+      console.log(`This directory does not exist, Creating now!!!`);
+      fs.mkdir(OUTPUT_DIR, (err) => (err) ? console.log(err) : writeHTML());
+
+    } else {
+      // else we just create the html file 
+      writeHTML();
+    }
+  })
+}
+
+// function that writes the html
+function writeHTML() {
+  let html = render(teamMembers)
+
+  fs.writeFile(outputPath, html, (err) => {
+      (err) ? console.log(err) : console.log('The file has been written succesfully!');
+  });
+}
+
+
+Question();
+
+// //Github
+// {
+//   type: "input",
+//     name: "github",
+//       message: "What is your Github username?"
+// },
+//       ]).then(function (engineerRes) {
+//   var newEngineer = new Engineer(engineerRes.name, engineerRes.email, uniqueId, engineerRes.github);
+//   uniqueId = uniqueId++;
+//   console.log(newEngineer);
+//   members.push(newEngineer);
+//   addUser();
+// })
+//     } else if (res.role === "Intern") {
+//   inquirer.prompt([
+//     {
+//       name: "name",
+//       message: "What is your name?",
+//       type: "input"
+//     },
+//     {
+//       name: "email",
+//       type: "input",
+//       message: "What is your email?"
+//     },
+//     {
+//       name: "school",
+//       type: "input",
+//       message: "Where did you graduate from college?"
+//     }
+//   ]).then(function (internRes) {
+//     var newIntern = new Intern(internRes.name, internRes.email, uniqueId, internRes.school);
+//     uniqueId = uniqueId++;
+//     console.log(newIntern)
+//     members.push(newIntern);
+//     addUser();
+//   });
+// } else if (res.role === "Manager") {
+//   inquirer.prompt([
+//     {
+//       name: "name",
+//       message: "What is your name?",
+//       type: "input"
+//     },
+//     {
+//       name: "email",
+//       type: "input",
+//       message: "What is your email?"
+//     },
+//     
+//   ]).then(function (managerRes) {
+//     var newManager = new Manager(managerRes.name, managerRes.email, uniqueId, managerRes.office);
+//     uniqueId = uniqueId++;
+//     console.log(newManager);
+//     members.push(newManager);
+//     addUser();
+//   });
+// };
+
+//   })
+//     .catch (function (err) {
+//   console.log(err);
+// })
+
+// };
+
+// //prompts user if they want to add another team member
+// function addUser() {
+//   inquirer.prompt([
+//     {
+//       name: "continue",
+//       message: "Do you want to add another team member?",
+//       type: "confirm"
+//     }
+//   ]).then(function (confirmRes) {
+//     confirmRes.continue ? employeePrompt() : generateHTML()
+//   })
+// };
+
+// function generateHTML() {
+//   console.log(members);
+//   function renderManager() {
+//   };
+//   function renderIntern() {
+//   };
+//   function renderEngineer() {
+//   };
+// }
+
+
+
+// employeePrompt();
+
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -97,4 +282,4 @@ employeePrompt();
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+// for the provided `render` function to work!
